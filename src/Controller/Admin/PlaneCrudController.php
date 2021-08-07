d23a4990b0411f410a11dc2f13aa9e3b389c45a2
Vladislav Safronov <dont7panic@gmail.com>
2021-08-07 23:15:37 +0600

    Modify plane add fields

--- src/Controller/Admin/PlaneCrudController.php
+++ src/Controller/Admin/PlaneCrudController.php
@@ -4,22 +4,19 @@ namespace App\Controller\Admin;
 
 use App\Entity\Plane;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class PlaneCrudController extends AbstractCrudController
 {
-    public static function getEntityFqcn(): string
-    {
-        return Plane::class;
-    }
+  public static function getEntityFqcn(): string {
+    return Plane::class;
+  }
 
-    /*
-    public function configureFields(string $pageName): iterable
-    {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
-    }
-    */
+  public function configureFields(string $pageName): iterable {
+    return [
+      TextField::new('name'),
+      IntegerField::new('seats')
+    ];
+  }
 }
