bfe665124ecb8c859cd177a51c775c88e88712ca
Vladislav Safronov <dont7panic@gmail.com>
2021-08-10 19:43:57 +0600

    Add flight validation in plane field

--- src/Form/FlightType.php
+++ src/Form/FlightType.php
@@ -3,12 +3,25 @@
 namespace App\Form;
 
 use App\Entity\Flight;
+use App\Entity\Plane;
+use App\Repository\FlightRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use DateInterval;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Callback;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 class FlightType extends AbstractType
 {
+  private $em;
+
+  public function __construct(ManagerRegistry $registry) {
+    $this->em = $registry;
+  }
+
   public function buildForm(FormBuilderInterface $builder, array $options) {
     $builder
       ->add('departsAt')
@@ -17,7 +30,26 @@ class FlightType extends AbstractType
 //            ->add('updatedAt')
       ->add('airportFrom')
       ->add('airportTo')
-      ->add('plane');
+      ->add('plane', EntityType::class, [
+        'class' => Plane::class,
+        'constraints' => [new Callback([$this, 'checkPlaneAccessibility'])]
+      ]);
+  }
+
+  public function checkPlaneAccessibility($obj, ExecutionContextInterface $context) {
+    $plane = $context->getRoot()->getData()->getPlane();
+    $departsAt = $context->getRoot()->getData()->getDepartsAt();
+    $arrivesAt = $departsAt->add(new DateInterval('PT4H'))->format('Y-m-d H:i:s');
+
+    $fr = $this->em->getRepository(Flight::class);
+    $res = $fr->findEngagedPlanes($plane, $departsAt->format('Y-m-d H:i:s'), $arrivesAt);
+
+    if (count($res)) {
+      $context
+        ->buildViolation('The plane has already been engaged!')
+        ->atPath('plane')
+        ->addViolation();
+    }
   }
 
   public function configureOptions(OptionsResolver $resolver) {
--- src/Repository/FlightRepository.php
+++ src/Repository/FlightRepository.php
@@ -14,37 +14,53 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class FlightRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
-    {
-        parent::__construct($registry, Flight::class);
-    }
+  public function __construct(ManagerRegistry $registry) {
+    parent::__construct($registry, Flight::class);
+  }
 
-    // /**
-    //  * @return Flight[] Returns an array of Flight objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('f')
-            ->andWhere('f.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('f.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
+  /**
+   * @return Flight[] Returns an array of Flight objects
+   */
 
-    /*
-    public function findOneBySomeField($value): ?Flight
-    {
-        return $this->createQueryBuilder('f')
-            ->andWhere('f.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
+  public function findEngagedPlanes($plane, $departsAt, $arrivesAt) {
+    return $this->createQueryBuilder('f')
+      ->where('f.plane = :plane')
+      ->andWhere('f.departsAt < :departsAt AND f.arrivesAt > :departsAt')
+      ->orWhere('f.departsAt < :arrivesAt AND f.arrivesAt > :arrivesAt')
+      ->orWhere('f.departsAt = :departsAt AND f.arrivesAt = :arrivesAt')
+      ->setParameter('plane', $plane)
+      ->setParameter('departsAt', $departsAt)
+      ->setParameter('arrivesAt', $arrivesAt)
+      ->getQuery()
+      ->getResult();
+  }
+
+  // /**
+  //  * @return Flight[] Returns an array of Flight objects
+  //  */
+  /*
+  public function findByExampleField($value)
+  {
+      return $this->createQueryBuilder('f')
+          ->andWhere('f.exampleField = :val')
+          ->setParameter('val', $value)
+          ->orderBy('f.id', 'ASC')
+          ->setMaxResults(10)
+          ->getQuery()
+          ->getResult()
+      ;
+  }
+  */
+
+  /*
+  public function findOneBySomeField($value): ?Flight
+  {
+      return $this->createQueryBuilder('f')
+          ->andWhere('f.exampleField = :val')
+          ->setParameter('val', $value)
+          ->getQuery()
+          ->getOneOrNullResult()
+      ;
+  }
+  */
 }
