0b80a0c098efe4c313656e9de4ad27525c5d6e38
Vladislav Safronov <dont7panic@gmail.com>
2021-08-11 20:51:13 +0600

    Add flash messages

--- src/Controller/Admin/FlightController.php
+++ src/Controller/Admin/FlightController.php
@@ -13,70 +13,79 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/admin/flight')]
 class FlightController extends AbstractController
 {
-    #[Route('/', name: 'admin_flight_index', methods: ['GET'])]
-    public function index(FlightRepository $flightRepository): Response
-    {
-        return $this->render('admin/flight/index.html.twig', [
-            'flights' => $flightRepository->findAll(),
-        ]);
+  #[Route('/', name: 'admin_flight_index', methods: ['GET'])]
+  public function index(FlightRepository $flightRepository): Response {
+    return $this->render('admin/flight/index.html.twig', [
+      'flights' => $flightRepository->findAll(),
+    ]);
+  }
+
+  #[Route('/new', name: 'admin_flight_new', methods: ['GET', 'POST'])]
+  public function new(Request $request): Response {
+    $flight = new Flight();
+    $form = $this->createForm(FlightType::class, $flight);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+      $entityManager = $this->getDoctrine()->getManager();
+      $entityManager->persist($flight);
+      $entityManager->flush();
+
+      $this->addFlash(
+        'success',
+        'Your changes were saved! New flight with id ' . $flight->getId() . ' has been created'
+      );
+      return $this->redirectToRoute('admin_flight_index', [], Response::HTTP_SEE_OTHER);
     }
 
-    #[Route('/new', name: 'admin_flight_new', methods: ['GET', 'POST'])]
-    public function new(Request $request): Response
-    {
-        $flight = new Flight();
-        $form = $this->createForm(FlightType::class, $flight);
-        $form->handleRequest($request);
+    return $this->renderForm('admin/flight/new.html.twig', [
+      'flight' => $flight,
+      'form' => $form,
+    ]);
+  }
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($flight);
-            $entityManager->flush();
+  #[Route('/{id}', name: 'admin_flight_show', methods: ['GET'])]
+  public function show(Flight $flight): Response {
+    return $this->render('admin/flight/show.html.twig', [
+      'flight' => $flight,
+    ]);
+  }
 
-            return $this->redirectToRoute('admin_flight_index', [], Response::HTTP_SEE_OTHER);
-        }
+  #[Route('/{id}/edit', name: 'admin_flight_edit', methods: ['GET', 'POST'])]
+  public function edit(Request $request, Flight $flight): Response {
+    $form = $this->createForm(FlightType::class, $flight);
+    $form->handleRequest($request);
 
-        return $this->renderForm('admin/flight/new.html.twig', [
-            'flight' => $flight,
-            'form' => $form,
-        ]);
+    if ($form->isSubmitted() && $form->isValid()) {
+      $this->getDoctrine()->getManager()->flush();
+
+      $this->addFlash(
+        'success',
+        'Your changes were saved! New flight with id ' . $flight->getId() . ' has been edited'
+      );
+
+      return $this->redirectToRoute('admin_flight_index', [], Response::HTTP_SEE_OTHER);
     }
 
-    #[Route('/{id}', name: 'admin_flight_show', methods: ['GET'])]
-    public function show(Flight $flight): Response
-    {
-        return $this->render('admin/flight/show.html.twig', [
-            'flight' => $flight,
-        ]);
+    return $this->renderForm('admin/flight/edit.html.twig', [
+      'flight' => $flight,
+      'form' => $form,
+    ]);
+  }
+
+  #[Route('/{id}', name: 'admin_flight_delete', methods: ['POST'])]
+  public function delete(Request $request, Flight $flight): Response {
+    if ($this->isCsrfTokenValid('delete' . $flight->getId(), $request->request->get('_token'))) {
+      $entityManager = $this->getDoctrine()->getManager();
+      $entityManager->remove($flight);
+      $entityManager->flush();
+
+      $this->addFlash(
+        'success',
+        'Your changes were saved! New flight with id ' . $flight->getId() . ' has been deleted'
+      );
     }
 
-    #[Route('/{id}/edit', name: 'admin_flight_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Flight $flight): Response
-    {
-        $form = $this->createForm(FlightType::class, $flight);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('admin_flight_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('admin/flight/edit.html.twig', [
-            'flight' => $flight,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'admin_flight_delete', methods: ['POST'])]
-    public function delete(Request $request, Flight $flight): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$flight->getId(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($flight);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('admin_flight_index', [], Response::HTTP_SEE_OTHER);
-    }
+    return $this->redirectToRoute('admin_flight_index', [], Response::HTTP_SEE_OTHER);
+  }
 }
