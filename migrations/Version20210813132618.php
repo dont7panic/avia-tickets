0ff61d1c84984df6e21f575e5b7cfc6c07c34337
Vladislav Safronov <dont7panic@gmail.com>
2021-08-13 19:41:34 +0600

    Modify Ticket entity

---
+++ migrations/Version20210813132618.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210813132618 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE ticket ADD created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', ADD updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE ticket DROP created_at, DROP updated_at');
+    }
+}
--- src/Entity/Ticket.php
+++ src/Entity/Ticket.php
@@ -3,75 +3,118 @@
 namespace App\Entity;
 
 use App\Repository\TicketRepository;
+use DateTimeImmutable;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=TicketRepository::class)
+ * @ORM\HasLifecycleCallbacks()
  */
 class Ticket
 {
-    /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+  /**
+   * @ORM\Id
+   * @ORM\GeneratedValue
+   * @ORM\Column(type="integer")
+   */
+  private $id;
 
-    /**
-     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="tickets")
-     * @ORM\JoinColumn(nullable=false)
-     */
-    private $user;
+  /**
+   * @ORM\ManyToOne(targetEntity=User::class, inversedBy="tickets")
+   * @ORM\JoinColumn(nullable=false)
+   */
+  private $user;
 
-    /**
-     * @ORM\ManyToOne(targetEntity=Flight::class, inversedBy="tickets")
-     * @ORM\JoinColumn(nullable=false)
-     */
-    private $flight;
+  /**
+   * @ORM\ManyToOne(targetEntity=Flight::class, inversedBy="tickets")
+   * @ORM\JoinColumn(nullable=false)
+   */
+  private $flight;
 
-    /**
-     * @ORM\Column(type="string", length=100, nullable=true)
-     */
-    private $status;
+  /**
+   * @ORM\Column(type="string", length=100)
+   */
+  #[Assert\Choice(['booked', 'paid'])]
+  private $status;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+  /**
+   * @ORM\Column(type="datetime_immutable")
+   */
+  private $createdAt;
 
-    public function getUser(): ?User
-    {
-        return $this->user;
-    }
+  /**
+   * @ORM\Column(type="datetime_immutable", nullable=true)
+   */
+  private $updatedAt;
 
-    public function setUser(?User $user): self
-    {
-        $this->user = $user;
+  public function getId(): ?int {
+    return $this->id;
+  }
 
-        return $this;
-    }
+  public function getUser(): ?User {
+    return $this->user;
+  }
 
-    public function getFlight(): ?Flight
-    {
-        return $this->flight;
-    }
+  public function setUser(?User $user): self {
+    $this->user = $user;
 
-    public function setFlight(?Flight $flight): self
-    {
-        $this->flight = $flight;
+    return $this;
+  }
 
-        return $this;
-    }
+  public function getFlight(): ?Flight {
+    return $this->flight;
+  }
 
-    public function getStatus(): ?string
-    {
-        return $this->status;
-    }
+  public function setFlight(?Flight $flight): self {
+    $this->flight = $flight;
 
-    public function setStatus(?string $status): self
-    {
-        $this->status = $status;
+    return $this;
+  }
 
-        return $this;
-    }
+  public function getStatus(): ?string {
+    return $this->status;
+  }
+
+  public function setStatus(?string $status): self {
+    $this->status = $status;
+
+    return $this;
+  }
+
+  public function getCreatedAt(): ?DateTimeImmutable {
+    return $this->createdAt;
+  }
+
+  public function setCreatedAt(DateTimeImmutable $createdAt): self {
+    $this->createdAt = $createdAt;
+
+    return $this;
+  }
+
+  public function getUpdatedAt(): ?DateTimeImmutable {
+    return $this->updatedAt;
+  }
+
+  public function setUpdatedAt(?DateTimeImmutable $updatedAt): self {
+    $this->updatedAt = $updatedAt;
+
+    return $this;
+  }
+
+  /**
+   * @ORM\PrePersist
+   */
+
+  public function setPrePersistValues(): void {
+    $this->setCreatedAt(new DateTimeImmutable('now'));
+  }
+
+  /**
+   * @ORM\PreUpdate
+   */
+
+  public function setUpdatedTimestamp(): void {
+    $this->setUpdatedAt(new DateTimeImmutable('now'));
+  }
 }
